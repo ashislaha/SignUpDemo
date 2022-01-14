539d5301e37a47ce6e3aa1735b5e86c006325cb8
ashislaha <mail2ashislaha@gmail.com>
2022-01-14 09:00:50 +0530

    project created

---
+++ SignUpDemo/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpDemo
+//
+//  Created by Ashis Laha on 14/01/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .green
+		print("controller loaded")
+	}
+}
+
